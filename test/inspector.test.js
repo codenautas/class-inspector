4c2cd5db1866b7de2c08656ff28f5d23b3e1d095
Sean Dou <douguohui@gmail.com>
2016-05-06 18:01:51 +0800

    sample class

--- test/inspector.test.js
+++ test/inspector.test.js
@@ -1,3 +1,35 @@
-describe('inspector', function() {
+const inspector = require('./../src');
 
+class Creature {
+  liveOn() {
+    return 'earth';
+  }
+}
+
+class Animal extends Creature {
+  static counts() {
+    return 10;
+  }
+  run() {}
+}
+
+class Dog extends Animal {
+  static counts() {
+    return 20;
+  }
+  static newOne() {
+    return this();
+  }
+  bark() {
+    return 'ouah-ouah';
+  }
+  get age() {
+    return 1;
+  }
+}
+
+describe('inspector', function() {
+  it('should return all methods', function() {
+
+  });
 });
